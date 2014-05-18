5ee395680b3137fa5111b63ddfc79062a7262b0b
GeoffGeis <geoffgeis@hotmail.com>
2014-05-17 23:58:41 -0500

    updates

--- hangman_2.rb
+++ hangman_2.rb
@@ -1,81 +1,28 @@
-class Sentence
-  attr_reader :puzzle, :sentence, :searchable_sentence
-  
-  def initialize(sentence)
-      @sentence = sentence
-      @searchable_sentence = sentence.split('')
-      @puzzle = blank_out(sentence)
-  end
-  
-  def get_indexes(char)
-    @searchable_sentence.each_index.select do |i| 
-      @searchable_sentence[i] == char 
-    end
-  end
-  
-  def replace_blanks(indexes,char)
-    indexes.each do |index|
-      @puzzle[index] = char
-    end
-  end
-  
-  def blank_out(sentence)
-    @searchable_sentence.map do |char| 
-      if char == " " 
-        " " 
-      else
-        "_" 
-      end
-    end
-  end
-  
-end
-
-#################################################################
+require './words/words.rb'; './sentence/sentence.rb'
 
 class Hangman
-  
-  def initialize
-    d = Words.new
-    @sentences = d.sentence
-    @game_sentence = Sentence.new(@sentences)
-    @guessed = []
-    @tries = 6
-  end
-
-  def do_turn
-    puts "Gimme a fucking guess:"
-    guess = gets.chomp.downcase
-    while @guessed.include?(guess)
-      puts "You used that one already asshat"
-      guess = gets.chomp.downcase
-    end
-    @guessed << guess 
-    if check_guess(guess)
-      true
-    else
-      false
-    end
-  end
-
   def do_game
-    while @tries != 0
+    d = Words.new
+    @game_sentence = Sentence.new(d)
+    guessed = []
+    tries = 6
+    while tries != 0
       if win?
         break
       end
       puts @game_sentence.puzzle.join(" ")
-      puts "You have already guessed: #{@guessed.join(" ")}"
-      puts "You have #{@tries} tries motherfucker!"
+      puts "You have already guessed: #{guessed.join(" ")}"
+      puts "You have #{tries} tries motherfucker!"
       if do_turn
         puts "Good guess!"
         next
       else
         puts "Failure is closer, bad guess"
-        @tries -= 1
+        tries -= 1
         next
       end
     end
-    if @tries == 0
+    if tries == 0
       puts @game_sentence.searchable_sentence.join(" ")
       puts "Game over man....game over!"
       continue?
@@ -93,12 +40,19 @@ class Hangman
       true
     end
   end
-    
-  def continue?
-    puts "Another you bitch? (y/n)"
-    choice = gets.chomp.downcase
-    if choice == "y"
-      reset
+
+  def do_turn
+    puts "Gimme a fucking guess:"
+    guess = gets.chomp.downcase
+    while guessed.include?(guess)
+      puts "You used that one already asshat"
+      guess = gets.chomp.downcase
+    end
+    guessed << guess 
+    if check_guess(guess)
+      true
+    else
+      false
     end
   end
 
@@ -111,116 +65,19 @@ class Hangman
       return true
     end
   end
+    
+  def continue?
+    puts "Another you bitch? (y/n)"
+    choice = gets.chomp.downcase
+    if choice == "y"
+      reset
+    end
+  end
 
   def reset
-    d = Words.new
-    @sentences = d.sentence
-    @game_sentence = Sentence.new(@sentences)
-    @guessed = []
-    @tries = 6
     do_game
   end
-  
 end
-
-#################################################################
-
-class Words
-
-  def initialize
-    name = []
-    adjective = []
-    noun = []
-    name << "sophia"
-    name << "aiden"
-    name << "emma"
-    name << "jackson"
-    name << "olivia"
-    name << "ethan"
-    name << "isabella"
-    name << "liam"
-    name << "ava"
-    name << "mason"
-    name << "lily"
-    name << "noah"
-    name << "zoe"
-    name << "lucas"
-    name << "chloe"
-    name << "jacob"
-    name << "mia"
-    name << "jayden"
-    name << "madison"
-    name << "jack"
-    name << "emily"
-    name << "logan"
-    name << "ella"
-    name << "ryan"
-    name << "madelyn"
-    name << "george"
-    adjective << "adorable"
-    adjective << "beautiful"
-    adjective << "clean"
-    adjective << "dark"
-    adjective << "elegant"
-    adjective << "fancy"
-    adjective << "glamorous"
-    adjective << "handsome"
-    adjective << "light"
-    adjective << "magnificent"
-    adjective << "plain"
-    adjective << "quaint"
-    adjective << "shiny"
-    adjective << "ugly"
-    adjective << "angry"
-    adjective << "bad"
-    adjective << "clumsy"
-    adjective << "creepy"
-    adjective << "dangerous"
-    adjective << "embarrassed"
-    adjective << "fierce"
-    adjective << "grumpy"
-    adjective << "itchy"
-    adjective << "jittery"
-    adjective << "nasty"
-    adjective << "crafty"
-    noun << "equipment"
-    noun << "screwdriver"
-    noun << "vegetable"
-    noun << "badge"
-    noun << "bead"
-    noun << "sun"
-    noun << "flame"
-    noun << "flower"
-    noun << "gate"
-    noun << "pharmacist"
-    noun << "weapon"
-    noun << "milkshake"
-    noun << "letter"
-    noun << "servant"
-    noun << "room"
-    noun << "nation"
-    noun << "river"
-    noun << "crack"
-    noun << "base"
-    noun << "mountain"
-    noun << "committee"
-    noun << "ray"
-    noun << "blood"
-    noun << "rain"
-    noun << "name"
-    noun << "train"
-    @name = name
-    @adjective = adjective
-    @noun = noun
-  end
-
-  def sentence
-    "#{@name.sample} is a #{@adjective.sample} #{@noun.sample}"
-  end
-
-end
-
-#################################################################
 
 h = Hangman.new
 h.do_game
