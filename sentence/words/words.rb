fde2f0ada7b224c649ba88e0229b7b202c5c2ef1
GeoffGeis <geoffgeis@hotmail.com>
2014-05-28 00:43:21 -0500

    shrunk words object

--- sentence/words/words.rb
+++ sentence/words/words.rb
@@ -1,89 +1,31 @@
 class Words
-  def initialize
-    name = []
-    adjective = []
-    noun = []
-    name << "sophia"
-    name << "aiden"
-    name << "emma"
-    name << "jackson"
-    name << "olivia"
-    name << "ethan"
-    name << "isabella"
-    name << "liam"
-    name << "ava"
-    name << "mason"
-    name << "lily"
-    name << "noah"
-    name << "zoe"
-    name << "lucas"
-    name << "chloe"
-    name << "jacob"
-    name << "mia"
-    name << "jayden"
-    name << "madison"
-    name << "jack"
-    name << "emily"
-    name << "logan"
-    name << "ella"
-    name << "ryan"
-    name << "madelyn"
-    name << "george"
-    adjective << "adorable"
-    adjective << "beautiful"
-    adjective << "clean"
-    adjective << "dark"
-    adjective << "elegant"
-    adjective << "fancy"
-    adjective << "glamorous"
-    adjective << "handsome"
-    adjective << "light"
-    adjective << "magnificent"
-    adjective << "plain"
-    adjective << "quaint"
-    adjective << "shiny"
-    adjective << "ugly"
-    adjective << "angry"
-    adjective << "bad"
-    adjective << "clumsy"
-    adjective << "creepy"
-    adjective << "dangerous"
-    adjective << "embarrassed"
-    adjective << "fierce"
-    adjective << "grumpy"
-    adjective << "itchy"
-    adjective << "jittery"
-    adjective << "nasty"
-    adjective << "crafty"
-    noun << "equipment"
-    noun << "screwdriver"
-    noun << "vegetable"
-    noun << "badge"
-    noun << "bead"
-    noun << "sun"
-    noun << "flame"
-    noun << "flower"
-    noun << "gate"
-    noun << "pharmacist"
-    noun << "weapon"
-    noun << "milkshake"
-    noun << "letter"
-    noun << "servant"
-    noun << "room"
-    noun << "nation"
-    noun << "river"
-    noun << "crack"
-    noun << "base"
-    noun << "mountain"
-    noun << "committee"
-    noun << "ray"
-    noun << "blood"
-    noun << "rain"
-    noun << "name"
-    noun << "train"
-    @name = name.sample
-    @adjective = adjective.sample
-    @noun = noun.sample
+  def initialize 
+    @name, @adjective, @noun = name, adjective, noun
+  end
+
+  def name
+    name = ["sophia", "aiden", "emma", "jackson", "olivia", 
+    "ethan", "isabella", "liam", "ava", "mason", "lily",
+    "noah", "zoe", "lucas", "chloe", "jacob", "mia", 
+    "jayden", "madison", "jack", "emily", "logan",
+    "ella", "ryan", "madelyn", "george"].sample
+  end
+
+  def adjective
+    adjective = ["adorable", "beautiful", "clean", "dark", "elegant",
+    "fancy", "glamorous", "handsome", "light", "magnificent",
+    "plain", "quaint", "shiny", "ugly", "angry", "bad", 
+    "clumsy", "creepy",  "dangerous", "embarrassed",
+    "fierce", "grumpy", "itchy", "jittery", 
+    "nasty", "crafty"].sample
+  end
+
+  def noun
+    noun = ["equipment", "screwdriver", "vegetable", "badge", "bead",
+    "sun", "flame", "flower", "gate", "pharmacist", "weapon",
+    "milkshake", "letter", "servant", "room", "nation", 
+    "river", "crack", "base", "mountain", "committee",
+    "ray", "blood", "rain", "name", "train"].sample
   end
 
   def sentence
